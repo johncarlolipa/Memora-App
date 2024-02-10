e943e328e33b07015cb0f6527331dfef8182ba3c
John Carlo Lipa <johncarlolipa@gmail.com>
2024-02-11 07:18:30 +0800

    setup server

---
+++ server/index.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+
+const app = express()
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+    res.json("hello")
+})
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running on Port: ${PORT}`)
+})
+
