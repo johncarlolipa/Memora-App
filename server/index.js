6e928cc53272d7e3943a0c5919ae0b69f19a4191
John Carlo Lipa <johncarlolipa@gmail.com>
2024-02-11 08:07:56 +0800

    setup CRUD API

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
 const connectDB = require("./connectDB");
-const Notes = require("./models/Notes")
+const Notes = require("./models/Notes");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -12,6 +12,77 @@ connectDB();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// routes
+// GET ALL
+app.get("/api/notes", async (req, res) => {
+  try {
+    const data = await Notes.find({});
+    if (!data) {
+      throw new Error("An error occured while fetching notes...");
+    }
+    res.status(201).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "An error occured while fetching notes..." });
+  }
+});
+
+// GET SINGLE NOTE
+app.get("/api/notes/:id", async (req, res) => {
+  try {
+    const noteID = req.params.id;
+    const data = await Notes.findById(noteID);
+    if (!data) {
+      throw new Error("Note not found");
+    }
+    res.status(201).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Note not found" });
+  }
+});
+
+// CREATE NOTE
+app.post("/api/notes", async (req, res) => {
+  try {
+    const { title, description } = req.body;
+    const data = await Notes.create({ title, description });
+    if (!data) {
+      throw new Error("An error occured while creating note...");
+    }
+    res.status(201).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "An error occured while creating note..." });
+  }
+});
+
+// UPDATE NOTE
+app.put("/api/notes/:id", async (req, res) => {
+  try {
+    const noteID = req.params.id;
+    const { title, description } = req.body;
+    const data = await Notes.findByIdAndUpdate(noteID, { title, description });
+    if (!data) {
+      throw new Error("Error occured while updating note");
+    }
+    res.status(201).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Error occured while updating note" });
+  }
+});
+
+// DELETE NOTE
+app.delete("/api/notes/:id", async (req, res) => {
+  try {
+    const noteID = req.params.id;
+    const data = await Notes.findByIdAndDelete(noteID);
+    if (!data) {
+      throw new Error("Error occured while deleting note");
+    }
+    res.status(201).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Error occured while deleting note" });
+  }
+});
 
 app.get("/", (req, res) => {
   res.json("hello");
