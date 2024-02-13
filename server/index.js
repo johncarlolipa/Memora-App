25fce96223dad9ec1a2e693d24378e7d6c73e15e
John Carlo Lipa <johncarlolipa@gmail.com>
2024-02-13 19:26:53 +0800

    add access control

--- server/index.js
+++ server/index.js
@@ -13,6 +13,11 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Private-Network', 'true');
+  next();
+});
+
 // routes
 
 // GET ALL
